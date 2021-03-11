f23f98ed05401b8b717324cb95e3eb4d8ab95a23
Noortje <noortjesteenbergen@gmail.com>
2021-03-11 12:46:03 +0100

    Contact php en css aangemaakt en foto's toegevoegd

---
+++ contact.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"><meta http-equiv="language" content="NL">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="keywords" content="">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+    	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"> 
+        <title>Contact</title>
+        <link rel="stylesheet" type="text/css" href="css/contact.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    </head>
+
+<body>
+  
+    <header style="height: 150px;">
+         <img src="images/logo.png" class="logo">
+         <nav>
+                <article class="nav">
+                    <nav>
+                       <ul id="menu">
+                        <a class="hoofdnav" href="#"><li>Home</li></a>
+                        <a class="hoofdnav" href="#"><li>About</li></a>
+                        <a class="hoofdnav" href="#"><li>Events</li></a>
+                        <a class="hoofdnav" href="#"><li>Contact</li></a>
+                      
+                      </ul>
+                  </nav>
+               </article>
+            </nav>
+    </header>
+
+    <main>
+
+        <section id="tekst1">
+            <p>FAQ</p>
+
+        </section>
+
+        <section id="container1">
+        <img src="images/phone-call.png" alt="">
+
+        </section>
+
+
+
+
+        <section id="container2">
+        <ul>
+        <li>Where is the event?</li>
+        <li>Where can u get drinks?</li>
+        <li>What time does the event start?</li>
+        <li>How much are the drinks?</li>
+        <li>Where can u get the tickets?</li>
+        </ul>
+        </section>
+
+    </main>
+
+    <footer>
+        <article class="footer">
+            <p class="footer-text">©Copyright Quick Boost 2021</p>
+        </article>
+    </footer>
+
+</body>
+
+</html>
+
