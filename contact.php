757bc7183ef7fa3f79b0038c4193087b8054cb41
Noortje <noortjesteenbergen@gmail.com>
2021-03-18 12:48:12 +0100

    tablet versie toegevoegd

--- contact.php
+++ contact.php
@@ -34,35 +34,76 @@
     <main>
 
 
-
-        <section id="container1">
-            <p><img id="icon1" src="images/phone-call.png" alt="">070-249-325</p>
-            <p><img id="icon1" src="images/email.png" alt="">quickboost@gmail.com</p>
-            <p><img id="icon1" src="images/pin.png" alt="">LA</p>
+<article class="bigscreen">
+    <section id="container1">
+        <p><img id="icon1" src="images/phone-call.png" alt="">070-249-325</p>
+        <p><img id="icon1" src="images/email.png" alt="">quickboost@gmail.com</p>
+        <p><img id="icon1" src="images/pin.png" alt="">LA</p>
             
 
-        </section>
+    </section>
 
 
-        <section id="container2">
-            <p>FAQ</p>
-            <ul>
-                <li>Where is the event?<br>The locations differ per event. You can find this information under the tab "events".</li>
-                <li>Where can u get drinks?<br>At the bar you can get al sort of beverages.</li>
-                <li>What time does the event start?<br>Each event starts at a different time. You can find this information under the tab "events".</li>
-                <li>With what do you pay for a drink?<br>U can buy special coins to pay for your drinks.</li>
-                <li>Where can u get the tickets?<br>U can find the tickets under the tab "events".</li>
-            </ul>
+    <section id="container2">
+        <p>FAQ</p>
+        <ul>
+            <li>Where is the event?<br>The locations differ per event. You can find this information under the tab "events".</li>
+            <li>Where can u get drinks?<br>At the bar you can get al sort of beverages.</li>
+            <li>What time does the event start?<br>Each event starts at a different time. You can find this information under the tab "events".</li>
+            <li>With what do you pay for a drink?<br>U can buy special coins to pay for your drinks.</li>
+            <li>Where can u get the tickets?<br>U can find the tickets under the tab "events".</li>
+        </ul>
 
-        <section>
+    <section>
             
-    </main>
 
-    <footer>
-        <article class="footer">
-            <p class="footer-text">©Copyright Quick Boost 2021</p>
-        </article>
-    </footer>
+</article>
+
+
+
+<article class="tablet">
+
+<section id="container3">
+        <p><img id="icon1" src="images/phone-call.png" alt="">070-249-325</p>
+        <p><img id="icon1" src="images/email.png" alt="">quickboost@gmail.com</p>
+        <p><img id="icon1" src="images/pin.png" alt="">LA</p>
+            
+
+    </section>
+
+
+    <section id="container4">
+        <p>FAQ</p>
+        <ul>
+            <li>Where is the event?<br>The locations differ per event. You can find this information under the tab "events".</li>
+            <li>Where can u get drinks?<br>At the bar you can get al sort of beverages.</li>
+            <li>What time does the event start?<br>Each event starts at a different time. You can find this information under the tab "events".</li>
+            <li>With what do you pay for a drink?<br>U can buy special coins to pay for your drinks.</li>
+            <li>Where can u get the tickets?<br>U can find the tickets under the tab "events".</li>
+        </ul>
+
+    <section>
+            
+
+
+
+</article>
+
+
+
+
+
+
+
+
+       
+</main>
+
+<footer>
+    <article class="footer">
+        <p class="footer-text">©Copyright Quick Boost 2021</p>
+    </article>
+</footer>
 
 </body>
 
